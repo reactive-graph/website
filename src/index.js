ef988036e430a94417f4b2802756847229b58d55
Andreas Schaeffer <andreas@schaeffernet.de>
2024-10-02 23:11:38 +0200

    Sticky header

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 import "../node_modules/@reactive-graph/design/build/css/reactive-graph.css";
 import "./base.css"
 import "./layout.css"
+import _ from "underscore";
+
+const checkHeader = _.throttle(() => {
+  // let scrollPosition = Math.round(window.scrollY);
+  if (window.scrollY > 100) {
+    document.querySelector("header").classList.add("sticky");
+  } else {
+    document.querySelector("header").classList.remove("sticky");
+  }
+}, 300);
+
+window.addEventListener("scroll", checkHeader)
